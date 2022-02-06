2d30815eb90bea622e75fa4168775c9d8f2b6be8
GC <gcoria1989@gmail.com>
2022-02-06 15:53:10 -0300

    desafio clase 6 6-2

--- Desafío clase 6/index.js
+++ Desafío clase 6/index.js
@@ -1,11 +1,58 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
+const fs = require('fs')
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-app.get('/', (req, res) => {
-    res.send(`<h1>Hola Otto y Ragnar</h1>
-            <h2>Ustedes son unos michis bonitos</h2>`)
+
+server.on('error', (err) => {
+    console.log(err.message)
 })
+
+class Contenedor{
+    constructor(name){
+        this.name = name
+    }
+
+    async getAll(){
+        const content = await fs.promises.readFile(this.name, 'utf8')
+        const contentObj = JSON.parse(content)
+        return contentObj
+    }
+
+    async getById(){
+        const content = await fs.promises.readFile(this.name, 'utf8')
+        const contentObj = JSON.parse(content)
+        console.log(contentObj)
+        const randomNumber= Math.floor(Math.random() * contentObj.length+1)
+        console.log(randomNumber)
+        const producto = contentObj[randomNumber-1]
+        return producto 
+    }
+}
+
+newContent = new Contenedor('productos.txt')
+
+app.get('/productos', (req, res) => {
+    newContent.getAll()
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+})
+
+app.get('/productoRandom', (req, res) => {
+    newContent.getById()
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        console.log(err.message)
+    }) 
+})
+
+
