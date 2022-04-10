9ae468d1a43a4a022fdb04ac802382d7ee63ad7c
GC <gcoria1989@gmail.com>
2022-04-10 20:55:35 -0300

    10-04

--- Desafio_clase_24/index.js
+++ Desafio_clase_24/index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 8080;
 const cookieParser = require('cookie-parser');
-const sesion = require('express-session');
-const FileStore = require('session-file-store')(sesion);
+const session = require('express-session');
+//const FileStore = require('session-file-store')(session);
+const MongoStore = require('connect-mongo');
 
 app.use(express.static('public'));
 app.use(cookieParser(['secret123abc']));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(sesion({
+app.use(session({
+    name: 'mySession',
     secret: 'secret123abc',
     resave: false,
     saveUninitialized: false,
-    store: new FileStore({
-        path: './sessions',
+    // store: new FileStore({
+    //     path: './sessions',
+    //     //ttl: 5,
+    //     retries: 0
+    // }),
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://gcoria1989:gimeno666@cluster0.c5lzx.mongodb.net/sesiones?retryWrites=true&w=majority',
     }),
     rolling: true,
     cookie:{
@@ -25,37 +32,34 @@ app.use(sesion({
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
     res.render('index');
+
+})
+
+app.post('/login', async (req, res) => {
+    const {username, email, password} = req.body;
+    //res.cookie('user', email, {httpOnly: true, signed: true});
+    res.cookie('username', username, {httpOnly: true, signed: true});
+    res.render('login', {username})
+    req.session.user = username;
+    req.session.save(() => {
+        console.log('sesion guardada');
+        console.log(req.session);
+    })
+})
+
+app.get('/logout', (req, res) => {
+    const username = req.signedCookies.username;
+    res.clearCookie('mySession');
+    res.render('logout', {username});
     req.session.destroy(err => {
         if (err) {
             console.log(err);
         }
-        else{
-            console.log('sesion destruida');
-        }
-})
-})
-
-app.post('/', (req, res) => {
-    const {username, email, password} = req.body;
-    res.cookie('user', email, {httpOnly: true, signed: true});
-    res.cookie('username', username, {httpOnly: true, signed: true});
-    res.render('login', {username})
-
-    req.session.save(() => {
-        console.log('sesion guardada');
+        console.log('sesion destruida');
     })
 })
-
-
-
-app.get('/logout', (req, res) => {
-    const username = req.signedCookies.username;
-    res.render('logout', {username});
-})
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
