5726513d7b07dfbf604671fff7abec2e61019d72
GC <gcoria1989@gmail.com>
2022-05-25 20:46:12 -0300

    25-5

--- Desafio clase 36/config.js
+++ Desafio clase 36/config.js
@@ -2,4 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     DB_PASSWORD: process.env.DB_PASSWORD,
+    ACCOUNT_SID: process.env.ACCOUNT_SID,
+    ACCOUNT_TOKEN: process.env.ACCOUNT_TOKEN
 };
--- Desafio clase 36/routes/carts/carts.routes.js
+++ Desafio clase 36/routes/carts/carts.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const twilio = require('twilio');
-const ACCOUNT_SID = 'AC416671f4800ffe34143d277644bed7e3';
-const ACCOUNT_TOKEN = 'ecc802beb88869221b82c338b12301eb';
+const ACCOUNT_SID = process.env.ACCOUNT_SID;
+const ACCOUNT_TOKEN = process.env.ACCOUNT_TOKEN;
 const twilioClient = new twilio(ACCOUNT_SID, ACCOUNT_TOKEN);
 const cart = require('../../data/cart')
 const newCart = cart[0].productsList;
